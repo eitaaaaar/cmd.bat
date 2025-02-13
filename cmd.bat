37fa39bd1129eaa7685f3ca326378dc48092de4b
eitaar <154091730+eitaar@users.noreply.github.com>
2025-02-13 18:41:52 +0000

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -1,10 +1,10 @@
 @echo off
 cd %USERPROFILE%
 title C:\Windows\System32\cmd.exe
-echo Microsoft Windows [Version 10.0.22631.4317]
+echo Microsoft Windows [Version 10.0.22631.4890]
 echo (c) Microsoft Corporation. All rights reserved.
 echo.
-echo made by eitaar: https://github.com/eitaaaaar/
+echo made by eitaar: https://github.com/eitaar/
 echo.
 :st
 for /f "usebackq" %%i in (`cd`) do set cd=%%i
